ee09fe33de4f5d3a0b2ad3bab97cf746bafabbde
renandelfino013 <renancontaps@gmail.com>
2025-01-18 19:07:50 -0300

    .

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,10 @@
+var velmax = 121;
+var carro = 120;
+console.log(`sua velocidade é de ${carro}km/h`);
+if (velmax <= carro) {
+        console.log("vc ultrapassou o limite de velocidade");
+        console.log("vc foi multado em r$ 2.000");
+} 
+else {
+    console.log("vc esta dentro do limite de velocidade");
+}
