bfc07e9dbd8fe10e9d970ea396c2d67564e8bef8
renandelfino013 <renancontaps@gmail.com>
2025-01-23 14:08:32 -0300

    ex015 finalizado

--- exercicios/ex015/script.js
+++ exercicios/ex015/script.js
@@ -3,25 +3,34 @@ var n2 = document.getElementById("n2");
 var passos = document.getElementById("passo");
 var resultado = document.getElementById("resposta");
 
-function somar() {}
-
 function contar() {
   var num1 = Number(n1.value);
   var num2 = Number(n2.value);
   var passo = Number(passos.value);
-    if (num1 + passo < num2){
-    for (num1; num1 < num2; num1 += passo) {
-        resultado.innerHTML += ` ${num1 + passo};`;}
-
-  }
+  resultado.innerHTML = "";
+  if (
+    n1.value.length == 0 ||
+    n2.value.length == 0 ||
+    passos.value.length == 0
+  ) {
+    alert("Todos os campos devem estar preenchidos!");
+    
+  } 
   
-}
- if (num1 + passo >= num2){
-    resultado.innerHTML += `Fim`
+ else if (passo == 0) {
+    alert("Passo inválido! Considerando ");
+   resultado.innerHTML = "passo invalido";
+ }
+  
+  
+  else if (num1 + passo < num2) {
+    for (var i = num1; i <= num2; i += passo) {
+      resultado.innerHTML += ` ${i} \u{1F603}`;
+    }
+  } else {
+    for (var i = num1; i >= num2; i -= passo) {
+      resultado.innerHTML += ` ${i} \u{1F603}`;
+    }
   }
-
- 
-
-
-
-
+  resposta.innerHTML += ` \u{1F3C1} Fim`;
+}
